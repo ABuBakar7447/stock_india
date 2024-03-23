d3108b3aa5097b3e4003fd8a7e5f5b3ed626dedf
ABuBakar7447 <lone7447@gmail.com>
2024-03-23 23:45:34 +0600

    some css property has changed

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -13,21 +13,21 @@ const Navbar = () => {
 
     return (
         <div
-            className={`lg:relative absolute h-screen duration-700 bg-[#1e375f] text-white ${openSideNav ? "md:w-2/4  w-3/5" : "w-0"} `}
+            className={`bg-[#1e375f] text-white lg:relative absolute h-screen duration-700 ${openSideNav ? "md:w-2/4  w-3/5" : "w-0"} `}
         >
 
-            <div className="absolute top-1/2 -right-6 -translate-y-1/2">
+            <div className="absolute -right-6 top-1/2 -translate-y-1/2">
                 <button
                     onClick={() => setOpenSideNav(!openSideNav)}
                     className="bg-[#1e375f] hover:bg-blue-800 duration-700 text-white h-24"
                 >
                     <IoMdArrowDroprightCircle
-                        className={` rounded ${openSideNav ? "w-6 h-16 rotate-180 duration-700" : "w-6 h-16 rotate-0 duration-700"}`}
+                        className={` rounded ${openSideNav ? "rotate-180 w-6 h-16 duration-700" : "rotate-0 w-6 h-16 duration-700"}`}
                     />
                 </button>
             </div>
 
-            <div className={`flex items-center justify-around min-h-16 duration-300 ${openSideNav ? "" : "hidden"}`}>
+            <div className={`flex justify-around items-center min-h-[64px] duration-300 ${openSideNav ? "" : "hidden"}`}>
 
                 <span className="flex items-center gap-2">
                     <RxAvatar className="w-8 h-8" />
@@ -44,7 +44,7 @@ const Navbar = () => {
             <hr />
 
             <div className={`${openSideNav ? "" : "hidden"} duration-500`}>
-                <div className="flex flex-col justify-around *:w-full *:px-5 *:py-2 my-5 font-semibold  hover:*:bg-slate-900 *:duration-300">
+                <div className="*:w-full flex flex-col justify-around *:px-5 *:py-2 my-5 font-semibold  hover:*:bg-slate-900 *:duration-300">
                     <Link href={"/"} className="flex justify-start items-center gap-2">
                         <MdOutlineMessage className="w-6 h-6" />Discussion Forum
                     </Link>
@@ -56,7 +56,7 @@ const Navbar = () => {
                     <Link href={"/"}>Sectors</Link>
                     <Link href={"/"}>Watchlist</Link>
                     <Link href={"/"}>Events</Link>
-                    <Link href={"/"}>News/Interview</Link>
+                    <Link href={"/"}>News / Interview</Link>
                 </div>
             </div>
         </div>
